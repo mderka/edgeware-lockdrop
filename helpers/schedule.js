b4c8c34be98347013fe64322fd583c09903223ff
Drew Stone <drewstone329@gmail.com>
2019-05-10 14:17:20 +0300

    use native less than operators

--- helpers/schedule.js
+++ helpers/schedule.js
@@ -18,17 +18,17 @@ const getAdditiveBonus = (lockTime, lockStart, currentTotalETH) => {
   if (toBN(lockStart) != toBN(JUNE_1ST_UTC)) {
     return getFixtureAdditiveBonus(lockTime, lockStart);
   } else {
-    if (toBN(lockTime) <= toBN(JUNE_16TH_UTC)) {
+    if (toBN(lockTime).lte(toBN(JUNE_16TH_UTC))) {
       return conditionalSwap(BONUS_50, currentTotalETH);
-    } else if (toBN(lockTime) <= toBN(JULY_1ST_UTC)) {
+    } else if (toBN(lockTime).lte(toBN(JULY_1ST_UTC))) {
       return conditionalSwap(BONUS_40, currentTotalETH);
-    } else if (toBN(lockTime) <= toBN(JULY_16TH_UTC)) {
+    } else if (toBN(lockTime).lte(toBN(JULY_16TH_UTC))) {
       return conditionalSwap(BONUS_30, currentTotalETH);
-    } else if (toBN(lockTime) <= toBN(JULY_31ST_UTC)) {
+    } else if (toBN(lockTime).lte(toBN(JULY_31ST_UTC))) {
       return conditionalSwap(BONUS_20, currentTotalETH);
-    } else if (toBN(lockTime) <= toBN(AUG_15TH_UTC)) {
+    } else if (toBN(lockTime).lte(toBN(AUG_15TH_UTC))) {
       return conditionalSwap(BONUS_10, currentTotalETH);
-    } else if (toBN(lockTime) <= toBN(AUG_30TH_UTC)) {
+    } else if (toBN(lockTime).lte(toBN(AUG_30TH_UTC))) {
       return toBN(0);
     } else {
       return toBN(0);
@@ -38,38 +38,31 @@ const getAdditiveBonus = (lockTime, lockStart, currentTotalETH) => {
 
 // FIXME: Ensure comparisons are correct which they ARE NOT
 const conditionalSwap = (bonus, currentTotalETH) => {
-  console.log(toWei('2200000', 'ether'), currentTotalETH.toString());
-  let below200K = (toBN(currentTotalETH) < toBN(toWei('200000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('200000', 'ether')).toString()} = ${below200K}`);
-  let below400K = (toBN(currentTotalETH) < toBN(toWei('400000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('400000', 'ether')).toString()} = ${below400K}`);
-  let below700K = (toBN(currentTotalETH) < toBN(toWei('700000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('700000', 'ether')).toString()} = ${below700K}`);
-  let below1100K = (toBN(currentTotalETH) < toBN(toWei('1100000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('1100000', 'ether')).toString()} = ${below1100K}`);
-  let below1600K = (toBN(currentTotalETH) < toBN(toWei('1600000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('1600000', 'ether')).toString()} = ${below1600K}`);
-  let below2200K = (toBN(currentTotalETH) < toBN(toWei('2200000', 'ether')));
-  console.log(`${toBN(currentTotalETH).toString()} < ${toBN(toWei('2200000', 'ether')).toString()} = ${below2200K}`);
+  let below200K = (toBN(currentTotalETH).lt(toBN(toWei('200000', 'ether'))));
+  let below400K = (toBN(currentTotalETH).lt(toBN(toWei('400000', 'ether'))));
+  let below700K = (toBN(currentTotalETH).lt(toBN(toWei('700000', 'ether'))));
+  let below1100K = (toBN(currentTotalETH).lt(toBN(toWei('1100000', 'ether'))));
+  let below1600K = (toBN(currentTotalETH).lt(toBN(toWei('1600000', 'ether'))));
+  let below2200K = (toBN(currentTotalETH).lt(toBN(toWei('2200000', 'ether'))));
   // For each condition, we take the minimum of the two bonuses
   if (below200K) {
-    return (bonus <= BONUS_50)
+    return (bonus.lte(BONUS_50))
       ? bonus
       : BONUS_50;
   } else if (below400K) {
-    return (bonus <= BONUS_40)
+    return (bonus.lte(BONUS_40))
       ? bonus
       : BONUS_40;
   } else if (below700K) {
-    return (bonus <= BONUS_30)
+    return (bonus.lte(BONUS_30))
       ? bonus
       : BONUS_30;
   } else if (below1100K) {
-    return (bonus <= BONUS_20)
+    return (bonus.lte(BONUS_20))
       ? bonus
       : BONUS_20;
   } else if (below1600K) {
-    return (bonus <= BONUS_10)
+    return (bonus.lte(BONUS_10))
       ? bonus
       : BONUS_10;
   } else if (below2200K) {
